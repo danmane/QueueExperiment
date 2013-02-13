cb992ad3d49c444ed66a16b034ad215e288feec9
Daniel Mané <danmane@gmail.com>
2013-02-13 01:47:14 -0600

    Test file added (for TDD)

---
+++ Java/QueueManagerTest.java
@@ -0,0 +1,14 @@
+class QueueManagerTest {
+	public static boolean runTests(){
+		return false;
+	}
+
+	public static void main(String[] args) {
+		if (runTests()){
+			System.out.println("Queue Manager tests passed");
+		}
+		else {
+			System.out.println("Queue Manager tests FAILED");
+		}
+	}
+}

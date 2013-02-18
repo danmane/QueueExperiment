962fbe78c9b0d03d7ebbb0904deecd6f9b60a239
Daniel Mané <danmane@gmail.com>
2013-02-18 13:51:56 -0600

    added more statistics to QueueManager

--- JavaQueueExperiment/src/QueueManager.java
+++ JavaQueueExperiment/src/QueueManager.java
@@ -4,65 +4,96 @@ import java.util.*;
 public class QueueManager {
 	// Simulates a queue (maybe the line at a bank) and records data. The queue has a sequence
 	// of tasks of variable length which are serviced FIFO. The manager has a fixed number of
-	// processors which can each independently handle one task at a time. The number of processors
+	// processors which can each independently handle one task at a time, and each can support an internal queue. The number of processors
 	// may change in response to queue conditions. 
-	// The QueueManager records two types of data. First, what is the length of the queue at each timestep.
-	// Second, how long does a new task appearing at each timestep have to wait before it
-	// reaches the head of the queue and is serviced. This data is collected via a 'phantom task'
-	// which is generated at each timestep. The 'phantom task' never is sent to a processor, but 
-	// when it arrives the time it waited since it was generated is recorded
 
 	private LinkedList<Task> queue; // Linked list of the queue of tasks to be completed
-	private int nProcessors; // current number of active processors 
 	private LinkedList<Processor> processors; // a list of every processor
-	private LinkedList<Processor> activeProcessors; // a list of active processors
-	private LinkedList<Processor> freeProcessors; // a list of unoccupied processors
 	private int currentTime; // the current timestep
-	private int closingTime; // time when the simulation stops adding new tasks
 	private int nextTaskID;
 	private int nextProcessorID;
 	
+	private LinkedList<Task> activeTasks; // All tasks that havent yet been processed
+	// implementation assumes strict FIFO which isn't true when using processor subqueues
+	
+	private int nProcessorTicks;
+	private int idleProcessorTicks;
+	private int activeProcessorTicks;
+	
+	private int totalNumTasks;
+	private int totalTimeTasks;
+	private int totalQueueTime;
+	
+	private int totalWaitTime;
+	private int maxWaitTime;
+	private int processedTasks;
+	private int activeProcessors;
 
-
-	public QueueManager(int nProcessors, int closingTime) { // constructor
+	public QueueManager(int nProcessors) { // constructor
 		currentTime = 0;
-		this.closingTime = closingTime;
 		nextTaskID = 0;
 		nextProcessorID = 0;
 		processors = new LinkedList<Processor>();
 		for (int i=0; i<nProcessors; i++){
 			addProcessor();
 		}
+		queue = new LinkedList<Task>();
+		activeTasks = new LinkedList<Task>();
+		
+		nProcessorTicks = 0;
+		idleProcessorTicks = 0;
+		activeProcessorTicks = 0;
+		totalNumTasks = 0;
+		totalTimeTasks = 0;
+		totalQueueTime = 0;
+		
+		totalWaitTime = 0;
+		maxWaitTime = 0;
+		processedTasks = 0;
+		activeProcessors = 0;
 		
 	}
-
-	public void tick(){ // Tick one second of the queue; returns number of people waiting
-		currentTime++;
-		addRandomTasks();
-		updateActiveProcessors();
+	public int tick(){ // Tick one second of the queue; returns the current time
 		assignJobs();
+		tickProcessors();
+		totalQueueTime += queue.size();
+		recordWaitTimes();
+		currentTime++;
+		return currentTime;
 	}
 
-	private void addRandomTasks(){ // randomly add tasks to the queue
-
-	}
-
-	private int randomTaskLength(){ // get a random length for a task
-		return 1;
-	}
-
-	private void updateActiveProcessors(){ // tick the time in each processor
-		// and move newly freed processors to the freeProcessor list
-		for (Processor p : processors){
-			boolean idle;
-			idle = p.tickTime(currentTime);
-			
+	private void recordWaitTimes(){
+		Task pTask;
+		
+		while (activeTasks.size() > 0 && activeTasks.peek().getProcessed()){
+			pTask = activeTasks.pop();
+			totalWaitTime += pTask.getWait();
+			processedTasks++;
+			maxWaitTime = Math.max(maxWaitTime, pTask.getWait());
 		}
-
+	}
+	
+	
+	private void tickProcessors(){ // tick the time in each processor
+		activeProcessors = 0;
+		for (Processor p : processors){
+			 if (p.isIdle()){
+				 idleProcessorTicks++;
+			 } else {
+				 activeProcessorTicks++;
+				 activeProcessors++;
+			 }
+			 p.tickTime(currentTime);
+		}
+		nProcessorTicks += processors.size();
 	}
 
 	private void assignJobs(){ // while there are free processors and tasks, assign tasks 
-
+		for (Processor p : processors){
+			if (p.isIdle() && queue.size() > 0){
+				p.takeTask(queue.pop(), currentTime);
+			}
+		}
 	}
 
 	public void addProcessor(){ // Add a processor
@@ -72,13 +103,62 @@ public class QueueManager {
 	}
 	
 	public void addTask(int duration){
+		if (duration == 0){
+			System.out.println("Bad task length");
+			return;
+		}
 		Task newTask = new Task(nextTaskID, currentTime, duration);
 		nextTaskID++;
 		queue.add(newTask);
+		activeTasks.add(newTask);
+		totalNumTasks++;
+		totalTimeTasks += duration;
+		
 	}
 
+	public boolean isFinished(){
+		return (queue.size() == 0 && activeProcessors == 0);
+	}
+	
 	public int removeProcessor() { // remove a processor - not implemented
 		return 1;
+	}
+	
+	public int getProcessedTasks(){
+		return processedTasks;
+	}
 
+	public int getCurrentTime() {
+		return currentTime;
+	}
+	public int getNumProcessors(){
+		return processors.size();
+	}
+	public int getQueueLength(){
+		return queue.size();
+	}
+	public int getnProcessorTicks() {
+		return nProcessorTicks;
+	}
+	public int getIdleProcessorTicks() {
+		return idleProcessorTicks;
+	}
+	public int getActiveProcessorTicks() {
+		return activeProcessorTicks;
+	}
+	public int getTotalNumTasks() {
+		return totalNumTasks;
+	}
+	public int getTotalTimeTasks() {
+		return totalTimeTasks;
+	}
+	public int getTotalQueueTime() {
+		return totalQueueTime;
+	}
+	public int getTotalWaitTime() {
+		return totalWaitTime;
+	}
+	public int getMaxWaitTime() {
+		return maxWaitTime;
 	}
 }
